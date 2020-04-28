f780c8029f8f8d3c79a8fe47ade33b23c44a1997
Yjang Wynter <21028927+YjangWynter@users.noreply.github.com>
2020-04-28 14:34:50 -0400

    PDO mysql connects

---
+++ staff.php
@@ -0,0 +1,283 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Staff Page Example </title>
+    <link rel="stylesheet" href="./css/staff.css">
+    <link rel="stylesheet" href="./css/staff.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+</head>
+
+<body>
+    <div class="container">
+
+
+
+
+        <section class="row col-sm-12 ">
+        <div class="col-sm-1"></div>
+            <div class="card col-sm-4 p-0 ml-4 mt-3 mb-3 mr-4" >
+
+                <div class=" card-header p-0">
+                    <img class="img-fluid card-img-top" src="./img/femaile_profile.png" width="50%">
+                </div>
+                <div class="card-footer bg-dark text-light" style="display:inline-block">
+                    <p class="card-title text-sm-center m-0 p-0" >Full Name MC Loren</p>
+                </div>
+                <div class="card-footer bg-secondary  d-flex justify-content-center align-items-center text-light">
+
+                    <ul style="list-style-type:none;" class="col-sm-12 mx-0 px-0">
+                        <li class="text-sm-left mx-3 row">
+                            <span><i class="fas fa-user mx-4"></i></span> Head Researcher
+                        </li>
+                        <li class="text-sm-left mx-3 row">
+                            <span><i class="fas fa-building mx-4"></i></span> Astronomy
+                        </li>
+                        <li class="text-sm-left mx-3 row">
+                            <span><i class="fas fa-envelope mx-4"></i></span> fullname.Loren@ucf.edu
+                        </li>
+                        <li class="text-sm-left mx-3 row">
+                            <span><i class="fas fa-phone-alt mx-4"></i></span> 123-456-7890
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-4 p-0 m-3">
+                <div class="research-interests">
+                    <div class="bg-dark text-light p-3 mt-5 mb-3 mx-1 text-center">
+                        <h5 class="upper">Research Areas</h5>
+                    </div>
+                    <ul class="text-black bg-white p-2 m-0 list-group list-group flush">
+                        <li class="list-group-item">Interest 1</li>
+                        <li class="list-group-item">Interest 2</li>
+                        <li class="list-group-item">Interest 3</li>
+                        <li class="list-group-item">Interest 4</li>
+                        <li class="list-group-item">Interest 5</li>
+
+                    </ul>
+                </div>           
+            </div>
+            
+            <div class="col-sm-1"></div>
+
+        </section>
+
+
+        <section class="row col-sm-12">
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-10 ">
+                        <div class=" bg-dark text-light p-3 mx-4 my-2  text-center">
+                            <h5 class="upper">Biography</h5>
+                        </div>
+                        <p class=" my-4 p-4">Magna in labore cupidatat consequat nulla dolor commodo laboris sint nulla ipsum ex consectetur.
+                            Nostrud velit culpa consequat amet eu. Nostrud tempor adipisicing ad minim occaecat sunt
+                            cupidatat est fugiat minim anim incididunt. Fugiat velit elit commodo exercitation elit
+                            consectetur magna enim. Laboris enim anim et nulla. >Magna in labore cupidatat consequat nulla dolor commodo laboris sint nulla ipsum ex consectetur.
+                            Nostrud velit culpa consequat amet eu. Nostrud tempor adipisicing ad minim occaecat sunt
+                            cupidatat est fugiat minim anim incididunt. Fugiat velit elit commodo exercitation elit
+                            consectetur magna enim. Laboris enim anim et nulla.</p>
+                            <p class=" my-4 p-4">Magna in labore cupidatat consequat nulla dolor commodo laboris sint nulla ipsum ex consectetur.
+                            Nostrud velit culpa consequat amet eu. Nostrud tempor adipisicing ad minim occaecat sunt
+                            cupidatat est fugiat minim anim incididunt. Fugiat velit elit commodo exercitation elit
+                            consectetur magna enim. Laboris enim anim et nulla.</p>
+                    </div>
+            <div class="col-sm-1"></div>
+        </div>
+
+
+        <div class="row col-sm-12">
+            <div class="col-sm-1"></div>
+                <div class="col-sm-10 p-3 m-2 justify-content-center">
+                <ul role="tablist" class="nav nav-tabs list-group-flush">
+                    <li class="list-group-item border bg-dark text-light p-3 text-center col-sm-3"role="presentation"><a class="text-light" href="#tab-1" role="tab" data-toggle="tab">
+                        <div class="icon-box-simple">
+                            <i class="icon-telescope"></i><span>Research Descriptions</span>
+                        </div>
+                        </a>
+                    </li>
+
+                    <li class="list-group-item border bg-dark text-light p-3  text-center col-sm-3"role="presentation" class=""><a  class="text-light" href="#tab-2" role="tab" data-toggle="tab">
+                            <div class="icon-box-simple">
+                                <i class="icon-telescope"></i><span>Publications</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="list-group-item border bg-dark text-light p-3  col-sm-3 text-center" role="presentation"><a  class="text-light" href="#tab-3" role="tab" data-toggle="tab">
+                            <div class="icon-box-simple">
+                                <i class="icon-puzzle"></i><span>Mentoring</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="list-group-item border bg-dark text-light p-3  col-sm-3 text-center" role="presentation">
+                        <a  class="text-light" href="#tab-4" role="tab" data-toggle="tab">
+                            <div class="icon-box-simple">
+                                <i class="ti-files"></i><span>Projects</span>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+                <div class="tab-content">
+                    <div id="tab-1" role="tabpanel" class="tab-pane active my-4 p-3">
+                    <br><p>Research Description</p>
+                        <p>  Magna in labore cupidatat consequat nulla dolor commodo laboris sint nulla ipsum ex consectetur.
+                        Nostrud velit culpa consequat amet eu. Nostrud tempor adipisicing ad minim occaecat sunt
+                        cupidatat est fugiat minim anim incididunt. Fugiat velit elit commodo exercitation elit
+                        consectetur magna enim. Laboris enim anim et nulla. Magna in labore cupidatat consequat nulla dolor commodo laboris sint nulla ipsum ex consectetur.
+                            Nostrud velit culpa consequat amet eu. Nostrud tempor adipisicing ad minim occaecat sunt
+                            cupidatat est fugiat minim anim incididunt. Fugiat velit elit commodo exercitation elit
+                            consectetur magna enim. Laboris enim anim et nulla.</p>
+                    </div>
+                    <div id="tab-2" role="tabpanel" class="tab-pane my-4 p-3">
+                    <br><p>Publications here|</p>
+
+                        <!--- --->
+                        <ul class="timeline">
+                            <li>
+                                <div class="timeline-item">
+                                    <h4>Publication 1 </h4>
+                                    <p><span>June 2014 - <b>Present</b></span>
+                                    </p>
+                                </div>
+                                <div class="timeline-description">
+                                    <h4>Title here</h4>
+                                    <p>Quisquam eveniet reprehenderit quis recusandae voluptatem debitis
+                                        corrupti hic dolorum ut commodi vero ab quasi nisi sit dolor
+                                        officiis
+                                        numquam, aspernatur fugiat!</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="timeline-item">
+                                    <h4>Publication 2</h4>
+                                    <p><span>May 2012 - <b>May 2014</b></span>
+                                    </p>
+                                </div>
+                                <div class="timeline-description">
+                                    <h4>Title here</h4>
+                                    <p>Quisquam eveniet reprehenderit quis recusandae voluptatem debitis
+                                        corrupti hic dolorum ut commodi vero ab quasi nisi sit dolor
+                                        officiis
+                                        numquam, aspernatur fugiat!</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="timeline-item">
+                                    <h4>Publication 3</h4>
+                                    <p><span>February 2010 - <b>April 2012</b></span>
+                                    </p>
+                                </div>
+                                <div class="timeline-description">
+                                    <h4>Title here</h4>
+                                    <p>Quisquam eveniet reprehenderit quis recusandae voluptatem debitis
+                                        corrupti hic dolorum ut commodi vero ab quasi nisi sit dolor
+                                        officiis
+                                        numquam, aspernatur fugiat!</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="timeline-item">
+                                    <h4>Publication 4</h4>
+                                    <p><span>September 2007 - <b>May 2010</b></span>
+                                    </p>
+                                </div>
+                                <div class="timeline-description">
+                                    <h4>Title here</h4>
+                                    <p>Quisquam eveniet reprehenderit quis recusandae voluptatem debitis
+                                        corrupti hic dolorum ut commodi vero ab quasi nisi sit dolor
+                                        officiis
+                                        numquam, aspernatur fugiat!</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="timeline-item">
+                                    <h4>Discovery 5</h4>
+                                    <p><span>September 2007 - <b>May 2010</b></span>
+                                    </p>
+                                </div>
+                                <div class="timeline-description">
+                                    <h4>Title here</h4>
+                                    <p>Quisquam eveniet reprehenderit quis recusandae voluptatem debitis
+                                        corrupti hic dolorum ut commodi vero ab quasi nisi sit dolor
+                                        officiis
+                                        numquam, aspernatur fugiat!</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="timeline-item">
+                                    <h4>Publication 6</h4>
+                                    <p><span>September 2007 - <b>May 2010</b></span>
+                                    </p>
+                                </div>
+                                <div class="timeline-description">
+                                    <h4>Title here</h4>
+                                    <p>Quisquam eveniet reprehenderit quis recusandae voluptatem debitis
+                                        corrupti hic dolorum ut commodi vero ab quasi nisi sit dolor
+                                        officiis
+                                        numquam, aspernatur fugiat!</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="timeline-item">
+                                    <h4>Publication 7</h4>
+                                    <p><span>September 2007 - <b>May 2010</b></span>
+                                    </p>
+                                </div>
+                                <div class="timeline-description">
+                                    <h4>Title here</h4>
+                                    <p>Quisquam eveniet reprehenderit quis recusandae voluptatem debitis
+                                        corrupti hic dolorum ut commodi vero ab quasi nisi sit dolor
+                                        officiis
+                                        numquam, aspernatur fugiat!</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="timeline-item">
+                                    <h4>Publication 8</h4>
+                                    <p><span>September 2007 - <b>May 2010</b></span>
+                                    </p>
+                                </div>
+                                <div class="timeline-description">
+                                    <h4>Title here</h4>
+                                    <p>Quisquam eveniet reprehenderit quis recusandae voluptatem debitis
+                                        corrupti hic dolorum ut commodi vero ab quasi nisi sit dolor
+                                        officiis
+                                        numquam, aspernatur fugiat!</p>
+                                </div>
+                            </li>
+                        </ul><!-- end of timeline-->
+
+                        <!-- -->
+                    </div>
+                    <div id="tab-3" role="tabpanel" class="tab-pane my-4 p-3">
+                    <br> <p>Mentoring information here</p>
+                    </div>
+                    <div id="tab-4" role="tabpanel" class="tab-pane my-4 p-3">
+                      <br>  <p>Publications here <br>
+                            ORCID page link:
+
+                            </p>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-sm-1"></div> 
+        </div>
+            <!-- end of row-->
+        </section>
+        <!-- End of Section -->
+    </div>
+        <!-- end of container-->
+           
+
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
